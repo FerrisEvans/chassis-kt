0ef9dbeffb7207b98aa5d5641d003f34c9db17e4
Ferris <ferris.evans@outlook.com>
2025-01-01 00:03:43 +0800

    datasource config

---
+++ app/src/main/kotlin/cn/dd/app/AppApplication.kt
@@ -0,0 +1,11 @@
+package cn.dd.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppApplication>(*args)
+}
